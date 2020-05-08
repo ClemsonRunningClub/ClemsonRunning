8fb16961fec0cdd5f0d9c39cf99367abd15bbc7b
David-Komf <krasowska89@gmail.com>
2020-05-08 02:15:05 -0400

    minor home updates

--- bucks/models.py
+++ bucks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    account_user  = models.CharField(max_length = 120)
+    miles         = models.DecimalField(decimal_places=2, max_digits=10000)
+    community     = models.DecimalField(decimal_places=2, max_digits=10000)
+    points        = models.DecimalField(decimal_places=2, max_digits=10000)
