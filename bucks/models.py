4553ba860b39084932f713ba97ba60679c8673f3
David-Komf <krasowska89@gmail.com>
2020-05-09 00:38:57 -0400

    home page update

--- bucks/models.py
+++ bucks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Product(models.Model):
+class Account(models.Model):
     account_user  = models.CharField(max_length = 120)
     miles         = models.DecimalField(decimal_places=2, max_digits=10000)
     community     = models.DecimalField(decimal_places=2, max_digits=10000)
